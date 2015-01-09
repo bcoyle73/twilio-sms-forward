371266c465373227603384a3716c705b60ef17cb
Brian Coyle <bcoyle73@gmail.com>
2015-01-09 16:12:21 -0500

    put a friendly instruction as home page

--- app.rb
+++ app.rb
@@ -4,6 +4,11 @@ require 'twilio-ruby'
 use Rack::TwilioWebhookAuthentication, ENV['AUTH_TOKEN'], '/message/forward'
 
 
+get '/' do
+ 	"You are good to go. Now put the url #{request.url}message/forward/?2025551212 into your Messaging URL on your phone number in the Twilio Account interface.
+ 	 <p>Obviously replacing with the phone number you want to forward to."
+end
+
 post '/message/forward/?' do
 	# take the forward number from the URL parameter 
 	# and the content of the message from the POST param
